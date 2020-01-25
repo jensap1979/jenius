65583521b82c83f963c81267e5702c51c59c2491
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-25 10:30:54 +0200

    21 done

--- part1/calculations/exercise_21/Program.cs
+++ part1/calculations/exercise_21/Program.cs
@@ -8,6 +8,18 @@ namespace exercise_21
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string firstInput = Console.ReadLine();
+
+      Console.WriteLine("Give the second number!");
+      string secondInput = Console.ReadLine();
+
+      int firstInt = Convert.ToInt32(firstInput);
+      int secondInt = Convert.ToInt32(secondInput);
+
+      double average = (firstInt + secondInt) / 2.0;
+
+      Console.WriteLine("The average is " + average);
 
     }
   }
