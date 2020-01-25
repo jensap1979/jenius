c0877abd66f648a8f8b13bd9a0b752dd124ff33c
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-25 11:01:37 +0200

    30 done

--- part1/conditionals/exercise_30/Program.cs
+++ part1/conditionals/exercise_30/Program.cs
@@ -7,6 +7,45 @@ namespace exercise_30
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give your percent [0 - 100]:");
+      string userInput = Console.ReadLine();
+
+      int percent = Convert.ToInt32(userInput);
+      
+
+      if (percent > 0 && percent < 50)
+      {
+        Console.WriteLine("Fail");
+
+      }
+      else if (percent > 49 && percent < 60)
+      {
+        Console.WriteLine("Grade: 1");
+      }
+      else if (percent > 59 && percent < 70)
+      {
+        Console.WriteLine("Grade: 2");
+      }
+      else if (percent > 69 && percent < 80)
+      {
+        Console.WriteLine("Grade: 3");
+      }
+      else if (percent > 79 && percent < 90)
+      {
+        Console.WriteLine("Grade: 4");
+      }
+      else if (percent > 89 && percent <= 100)
+      {
+        Console.WriteLine("Grade: 5");
+      }
+      else if (percent > 100)
+      {
+        Console.WriteLine("Outstanding!");
+      }
+      else
+      {
+        Console.WriteLine("Impossible");
+      }
 
     }
   }
