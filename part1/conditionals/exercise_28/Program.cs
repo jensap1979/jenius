bb89016a282725fa7d3e530931de9b75faf52fdc
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-25 10:55:04 +0200

    28 done

--- part1/conditionals/exercise_28/Program.cs
+++ part1/conditionals/exercise_28/Program.cs
@@ -7,6 +7,20 @@ namespace exercise_28
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("How old are you?");
+      string userInput = Console.ReadLine();
+
+      int userInt = Convert.ToInt32(userInput);
+
+      if (userInt >= 18)
+      {
+        Console.WriteLine("You're an adult!");
+
+      }
+      else
+      {
+        Console.WriteLine("You're under age!");
+      }  
 
     }
   }
