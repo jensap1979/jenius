e53c27e585810db3dd936fbeead102ee96fa2131
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-25 10:17:50 +0200

    exercise17

--- part1/calculations/exercise_17/Program.cs
+++ part1/calculations/exercise_17/Program.cs
@@ -8,6 +8,23 @@ namespace exercise_17
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string firstInput = Console.ReadLine();
+
+      Console.WriteLine("Give the second number!");
+      string secondInput = Console.ReadLine();
+
+      int firstInt = Convert.ToInt32(firstInput);
+      int secondInt = Convert.ToInt32(secondInput);
+
+      Console.WriteLine("The sum is " + (firstInt + secondInt));
+      
+
+
+
+      
+      
+  
 
     }
   }
