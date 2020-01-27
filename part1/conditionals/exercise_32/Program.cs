7b33df0ef90c7b89a1cc587118727b551d031bb9
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-27 10:31:58 +0200

    32 done

--- part1/conditionals/exercise_32/Program.cs
+++ part1/conditionals/exercise_32/Program.cs
@@ -6,7 +6,31 @@ namespace exercise_32
   {
     public static void Main(string[] args)
     {
+      
+      //Speak, friend, and enter!
+      //> Let meeeee in!
+      //They've got a cave troll!
+      //Speak, friend, and enter!
+      //> Mellon
+      //Welcome, friend
       // Write your code here:
+      Console.WriteLine("Speak, friend, and enter!");
+      string Mellon = Console.ReadLine();
+      
+      if (Mellon == "Mellon")
+      {
+      
+      Console.WriteLine("Welcome, friend!");
+      }
+
+      else
+      {
+      Console.WriteLine("They've got a cave troll!");    
+      }
+      
+      
+      
+      
 
     }
   }
