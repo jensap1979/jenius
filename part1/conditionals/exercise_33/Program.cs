940e0c393fe2f2b6dca82df7a4a75721e2f81541
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-27 11:09:25 +0200

    33 done

--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -6,7 +6,25 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
+      
       // Write your code here:
+      
+      Console.WriteLine("Give the first string:");
+      string ettan = Console.ReadLine();
+
+      Console.WriteLine("Give the second string:"); 
+      string ettan1 = Console.ReadLine();
+
+      if (ettan == ettan1) 
+      {
+      Console.WriteLine("Echo!");
+      }
+      
+      else
+      {
+      Console.WriteLine("Nope!");    
+      }    
+      
 
     }
   }
