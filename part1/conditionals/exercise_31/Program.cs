e4d785e0ee31d15cf1241c5dda8ffb19e25dc627
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-27 09:50:23 +0200

    31 done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string number = Console.ReadLine();
+      int cnt = Convert.ToInt16(number);
+      if ((cnt % 2) ==0)
+      {
+        Console.WriteLine("It is even.");
+      }
+      else
+      {
+      Console.WriteLine("It is odd.");  
 
-    }
+      }
+
+    } 
   }
 }
