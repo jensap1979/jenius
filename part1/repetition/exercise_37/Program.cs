623378fb7ff533b912dd5dd0ef1f2faab256d503
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-28 10:03:24 +0200

    37 done

--- part1/repetition/exercise_37/Program.cs
+++ part1/repetition/exercise_37/Program.cs
@@ -8,7 +8,23 @@ namespace exercise_37
     {
       int sum = 0;
       // Write your code here:
-
-    }
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int myNumber = Convert.ToInt32(Console.ReadLine());
+    
+        if (myNumber == 0)
+        {
+       break;
+        }
+        else 
+       {
+        sum++;
+        }
+     
+      }
+        Console.WriteLine("Total amount of numbers: " + sum);
+    
+   }
   }
 }
