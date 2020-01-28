87365a0c34ba7a9fae3a65b3f68c794ad13412b7
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-28 11:23:41 +0200

    38 broke

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -7,7 +7,26 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
-
+     int negnum = 0;
+     while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+      
+        
+        if (numero == 0)
+        {
+        break;
+        }
+        else if (numero < 0);
+        {
+         negnum = negnum +2;
+         }
+         
+         
+      
+      }
+      Console.WriteLine("Total amount of negative numbers: " + negnum);
     }
   }
 }
