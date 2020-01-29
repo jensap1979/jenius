9b45572b21235fcc59f9b132d3ea7432180d39b9
Jens Palmborg <jens.palmborg@centria.fi>
2020-01-29 10:20:17 +0200

    34 done

--- part1/repetition/exercise_34/Program.cs
+++ part1/repetition/exercise_34/Program.cs
@@ -6,7 +6,7 @@ namespace exercise_34
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      // Write code here:
     while (true) 
      {
       Console.WriteLine("Do you want to continue?");
