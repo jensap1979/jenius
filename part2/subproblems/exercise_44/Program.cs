796e664e573884a3ddff1064e2668a8f9993705f
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-01 13:32:07 +0200

    44 done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -8,6 +8,21 @@ namespace exercise_44
     {
 
       // Write your code here:
+          int num1 = Convert.ToInt32(Console.ReadLine());
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      if (num1 > num2)
+      {
+        Console.WriteLine(num1 + " is greater than " + num2 + ".");
+      }
+      else if (num1 < num2)
+      {
+        Console.WriteLine(num1 + " is less than " + num2 + ".");
+      }
+      else
+      {
+        Console.WriteLine(num1 + " is equal to "+ num2 + ".");
+      }
+
 
     }
   }
