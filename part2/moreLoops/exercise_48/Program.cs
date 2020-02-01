f7775efd7e8c08be12b29d834bee2303b40743f7
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-01 13:46:18 +0200

    48 broke

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -8,6 +8,27 @@ namespace exercise_48
     {
 
       // Write your code here:
+       int sum = 0;
+      Console.WriteLine("Give numbers:");
+      while (true)
+
+      { 
+        
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num ==-1)
+      {
+        Console.WriteLine("thx! Bye!");
+        break;
+      }
+      else
+      {
+        sum += num;
+        num++;
+        
+      }
+      
+      }
+      Console.WriteLine(" sum: " + sum);
  
     }
   }
