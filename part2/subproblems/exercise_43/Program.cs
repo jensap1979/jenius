d03bc5e7697a393bb53636de38b2088ea978f713
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-01 13:29:32 +0200

    43 broke

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -8,6 +8,20 @@ namespace exercise_43
     {
       // Write your code here:
       
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      if (num1 > num2)
+      {
+        Console.WriteLine(num1 + " is greater than " + num2 + ".");
+      }
+      else if (num1 < num2)
+      {
+        Console.WriteLine(num1 + " is less than " + num2 + ".");
+      }
+      else
+      {
+        Console.WriteLine(num1 + " is equal to "+ num2 + ".");
+      }
     }
   }
 }
