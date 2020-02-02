d1cfce9030deeefdbfc4f5401ac3b1b53050cb4d
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-02 19:34:40 +0200

    54 done

--- part2/methods/exercise_54/Program.cs
+++ part2/methods/exercise_54/Program.cs
@@ -5,12 +5,22 @@ namespace exercise_54
   class Program
   {
     public static void Main(String[] args)
-    {
+     {
       // Call your method here:
-
+      DivisibleByThreeInRange(3, 6);
     }
 
     // Write your method here:
-
+    public static void DivisibleByThreeInRange(int beginning, int end)
+    {
+      while (beginning <= end)
+      {
+        if (beginning % 3 == 0)
+        {
+          Console.WriteLine(beginning);
+        }
+          beginning++;
+      }
+    }
   }
 }
