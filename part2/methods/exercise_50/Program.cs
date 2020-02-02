b91a9b4fa139288008cbd2b2e50250e6b60483ff
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-02 19:22:40 +0200

    50 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -2,17 +2,27 @@
 
 namespace exercise_50
 {
-  class Program
+   class Program
   {
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      Console.WriteLine("How many times?");
+      int numOfTimes = Convert.ToInt32(Console.ReadLine());
+
+      int i = 0;
+      while (i < numOfTimes)
+      {
+        PrintPhrase();
+        i++;
+      }
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase() 
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
+
 }
