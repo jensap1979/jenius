7db396d369613431daae209640a980e5902ddc79
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-02 19:37:11 +0200

    55 broke

--- part2/methods/exercise_55/Program.cs
+++ part2/methods/exercise_55/Program.cs
@@ -5,11 +5,14 @@ namespace exercise_55
   class Program
   {
     public static void Main(String[] args)
-    {
-
+    { {
+      NumberUno();
     }
-
     // Write your method here:
+    public static int NumberUno()
+    {
+      return 1;
+    }
 
   }
 }
