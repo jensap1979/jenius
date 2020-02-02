5fb8499edfec7d3a5b5ce84fd05e5f89d2b45435
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-02 19:29:23 +0200

    52 done

--- part2/methods/exercise_52/Program.cs
+++ part2/methods/exercise_52/Program.cs
@@ -5,12 +5,19 @@ namespace exercise_52
   class Program
   {
     public static void Main(String[] args)
-    {
+      {
       // Call your method here:
-
+      PrintFromNumberToOne(9);
     }
 
     // Write your method here:
-
+    public static void PrintFromNumberToOne(int number) 
+    {
+      while (number > 0) 
+      {
+        Console.WriteLine(number);
+        number--;
+      }
+    }
   }
 }
