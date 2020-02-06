71fbd59ee76adfab0a40136ba24bae0957577907
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-06 13:56:36 +0200

    72 comment

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -7,7 +7,12 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
-
+         //////////ask while
+         ///ny kolumn int smallest
+         //go through list  save numer aS int number if number is smaller than smaLLest 
+         //smallest = number;
+         //for (int i = 0; number in list --if list[i] == smallest)
+         //writeline found at index +i;   
     }
   }
 }
