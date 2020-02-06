560fa6324629ec4fdefebbe684b3ee57be795e24
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-06 13:11:50 +0200

    90 underconst

--- part3/strings/exercise_90/Program.cs
+++ part3/strings/exercise_90/Program.cs
@@ -6,10 +6,25 @@ namespace exercise_90
   class Program
   {
     public static void Main(string[] args)
-    {
+        int oldest = 0;
+        while (true)
+        
+        {
+          string asked = Console.ReadLine();
+          if ( asked == "")
+          {
+          break;
+          }
+          string[] parts = asked.Split(",");
+          if (Convert.ToInt32(parts[1]) > oldest);  
+        
+          {
+         Oldest = Convert.ToInt32(Parts[1]);
+          }
 
-
+        Console.WriteLine("Age of the oldest " + oldest );
     }
+    
   }
 }
 
