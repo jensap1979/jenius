3a45a44a6c67422b48df965b3efe80fd59bfae29
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-07 11:29:32 +0200

    70 done

--- part3/lists/exercise_70/Program.cs
+++ part3/lists/exercise_70/Program.cs
@@ -15,11 +15,34 @@ namespace exercise_70
         {
           break;
         }
-
         list.Add(input);
-        Console.WriteLine("The greatest number:" + list);
+        if (input > 100)
+        {
+          break;
+        }
+        list.Add(input);
+        if (input == 50)
+        {
+          break;
+        }
+        list.Add(input);
+        ///extra just for fun
+      
+      
+      
       }
-    
+      int greatest = list[0];
+
+      for(int i = 0; i < list.Count; i++) 
+      {
+        int number = list[i];
+        if (greatest < number) 
+        {
+         greatest = number;
+        }
+      }
+      Console.WriteLine("The greatest number: " + greatest);
+
     }
   }
 }
