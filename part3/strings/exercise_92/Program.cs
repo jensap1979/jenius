12f7d0dc74a364abb32957b339968b1706e10b4b
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-11 09:51:51 +0200

    92 copy

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,7 +7,31 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+    int oldest = Convert.ToInt32(DateTime.Now.Year + 1);
+            string longestName = "";
 
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+
+                string[] pieces = input.Split(",");
+
+                if (oldest > Convert.ToInt32(pieces[1]))
+                {
+                    oldest = Convert.ToInt32(pieces[1]);
+                }
+
+                if (longestName.Length < pieces[0].Length)
+                {
+                    longestName = pieces[0];
+                }
+            }
+            Console.WriteLine("Longest name: " + longestName);
+            Console.WriteLine("Highest age: " + (Convert.ToInt32(DateTime.Now.Year) - oldest));  
     }
   }
 }
