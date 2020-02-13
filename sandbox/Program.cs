46c7af6cc67cc50878775d630614623b35b5a374
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-13 13:14:12 +0200

    new

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,29 +1,27 @@
 ﻿
 using System;
-using System.IO;
-
-class Text;
+namespace sandbox
 {
- private static void Main()
-   { 
-  string text = File.ReadAllText("text.txt");
+  class Program
+ {
+ public static void Main(string[] args)
+  { 
+    string text = ("text.txt");
 
   // Display the file contents to the console. Variable text is a string.
-  Console.WriteLine("This was done with ReadAllText.");
-  Console.WriteLine(text);
+   Console.WriteLine("This was done with ReadAllText.");
+    Console.WriteLine(text);
 
   //Print empty line for easier reading
-  Console.WriteLine();
+    Console.WriteLine();
 
-  // Example #2
-  // Read each line of the file into a string array. 
-  //Each element of the array is one line of the file.
-  Console.WriteLine("This was done with ReadAllLines.");
-  string[] lines = File.ReadAllLines("text.txt");
+
 
   // Display the file contents by using a foreach loop.
   foreach (string line in lines)
-  {
+    {
     Console.WriteLine(line);
+    }
+   
   }
 }
