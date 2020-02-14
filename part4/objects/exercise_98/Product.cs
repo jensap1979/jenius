7e67e56c972615b536d2ba99026d2b12e25613c3
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-14 13:55:54 +0200

    on way

---
+++ part4/objects/exercise_98/Product.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace exercise_98
+{
+  public class Product  
+
+  {
+    private string name;
+    private double price;
+    private int quantity;
+
+    public Product(string name, double price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        
+    }
+     {public void PrintProduct()
+
+     Console.Writeline(name + price * quantity); 
+     }
+     
+
+
+  }
+}
