3147994b0d615b75941f64f1101aa6a7d367cd83
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-14 11:23:13 +0200

    94 done

--- part4/objects/exercise_94/Program.cs
+++ part4/objects/exercise_94/Program.cs
@@ -8,7 +8,14 @@ namespace exercise_94
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account Heikki = new Account("Heikki's account", 1000);
+      Account Personal = new Account("Personal account", 0);
 
+      Heikki.Withdrawal(100);
+      Personal.Deposit(100);
+      
+      Console.WriteLine(Heikki);
+      Console.WriteLine(Personal);
     }
   }
 }
