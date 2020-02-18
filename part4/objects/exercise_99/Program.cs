02b9775ab6b12c054912dcd2058aabb518aafab7
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-18 08:55:23 +0200

    ok

--- part4/objects/exercise_99/Program.cs
+++ part4/objects/exercise_99/Program.cs
@@ -7,11 +7,15 @@ namespace exercise_99
     public static void Main(string[] args)
     {
 
+     
+
       DecreasingCounter counter = new DecreasingCounter(20);
       counter.PrintValue();
 
       counter.Reset();
       counter.PrintValue();
+
+
     }
   }
 }
