d463d51b2bcff37319a4f5cb43c81b999834b396
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-18 10:05:48 +0200

    ok

---
+++ part4/objects/exercise_101/Dalmatian.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace exercise_101
+{
+  public class Dalmatian
+    {
+    
+        public string name{ get; set;}
+        public int spots{ get; set; }
+        public Dalmatian(string name, int spots)
+         {
+          
+        
+            this.name = name;
+            this.spots = spots;
+
+
+         }
+
+    }
+}    
--- part4/objects/exercise_101/Program.cs
+++ part4/objects/exercise_101/Program.cs
@@ -6,7 +6,7 @@ namespace exercise_101
   {
     public static void Main(string[] args)
     {
-      Dalmatian spotty = new Dalmatian("Spot", 306);
+      Dalmatian spotty = new Dalmatian("Jürgen", 365);
       Console.WriteLine(spotty.name + " is a very good dog. He has " + spotty.spots + " darker spots in his fur");
     }
   }
