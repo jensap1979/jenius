6491a5103d25116113842d438d17ed0860ea6815
Jens Palmborg <jens.palmborg@centria.fi>
2020-02-18 09:26:19 +0200

    ok

--- part4/objects/exercise_100/Program.cs
+++ part4/objects/exercise_100/Program.cs
@@ -2,14 +2,26 @@
 
 namespace exercise_100
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
+            Debt mortgage = new Debt(120000.0, 1.01);
+            mortgage.PrintBalance();
+
+            mortgage.WaitOneYear();
+            mortgage.PrintBalance();
+
+            
+            int years = 0;
+            while (years < 1)
+            {
+                mortgage.WaitOneYear();
+                years = years + 1;
+            }
+
+            mortgage.PrintBalance();
+        }
     }
-  }
 }
-
-
-
