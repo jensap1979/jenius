02a6beec34706e81bf5aa5099216293e9d95e1d7
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-19 12:45:44 +0200

    ok

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -5,9 +5,9 @@ namespace exercise_53
   class Program
   {
     public static void Main(String[] args)
-       {
+    {
       // Call your method here:
-      Division(10, 6);
+      Division(10, 3);
     }
 
     // Write your method here:
