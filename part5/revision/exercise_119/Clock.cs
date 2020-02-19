0a3a1b899f2e0fc7f5ba749af3107e5e4b277e94
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-02-19 10:38:13 +0200

    upd

---
+++ part5/revision/exercise_119/Clock.cs
@@ -0,0 +1,36 @@
+namespace exercise_119
+{
+    public class Clock
+    {
+    private ClockHand hours;
+    private ClockHand minutes;
+    private ClockHand seconds;
+
+    public Clock()
+        {
+            this.hours = new ClockHand(24);
+            this.minutes = new ClockHand(60);
+            this.seconds = new ClockHand(60);
+        }
+
+        public void Advance()
+        {
+            this.seconds.Advance();
+
+            if (this.seconds.value == 0)
+            {
+            this.minutes.Advance();
+
+            if (this.minutes.value == 0)
+            {
+                this.hours.Advance();
+            }
+            }
+        }
+
+        public override string ToString()
+        {
+            return hours + ":" + minutes + ":" + seconds;
+        }
+    }
+}
--- part5/revision/exercise_119/Program.cs
+++ part5/revision/exercise_119/Program.cs
@@ -6,9 +6,23 @@ namespace exercise_119
 {
   class Program
   {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
+      ClockHand hours = new ClockHand(24);
+      ClockHand minutes = new ClockHand(60);
+      ClockHand seconds = new ClockHand(60);
 
+      while (true)
+      {
+        // 1. Printing the time
+        Console.WriteLine(hours + ":" + minutes + ":" + seconds);
+
+        // 2. Advancing the second hand
+        seconds.Advance();
+
+       
+          }
+      }
     }
   }
 }
