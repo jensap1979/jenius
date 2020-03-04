901b60474dbc02c4389da244232096c3765f1da4
Jens Palmborg SOFTK20P <jens.palmborg@centria.fi>
2020-03-04 09:20:09 +0200

    ok

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -5,8 +5,23 @@ namespace exercise_114
     class Program
     {
         public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            List<Person> persons = new List<Person>();
 
+            // a person object can be created first
+            Person john = new Person("John");
+            // and then added to the list
+            persons.Add(john);
+
+            // person objects can also be created "in the same sentence" that they are added to the list
+            persons.Add(new Person("Matthew"));
+            persons.Add(new Person("Martin"));
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person);
+            }
         }
     }
 }
