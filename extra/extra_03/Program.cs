3561381619d30329d165702496ce67f17f5c74fa
Jens Palmborg <jens.palmborg@centria.fi>
2020-03-12 15:30:11 +0200

    Thank you Marcus

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_03
 {
@@ -8,6 +9,50 @@ namespace extra_03
     {
       // Add your code here:
 
+
+      // numbers we want answers on
+      int sum = 0;
+      int total = 1; // can't multiply with zero
+      int userNumbers = 0;
+      double average = 0;
+
+      // ask user how many numbers
+      Console.WriteLine("How many numbers?");
+      int numAmount = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give " + numAmount + " numbers:");
+
+      // make list for the numbers
+      List<int> numbers = new List<int>();
+
+      // add users numbers to the list
+
+      // for (int i = 0; i < numAmount; i++)
+      // {
+      //   userNumbers = Convert.ToInt32(Console.ReadLine());
+      //   numbers.Add(userNumbers);
+      // }
+
+      // doing it with a while loop is a bit clearer, but for loop looks more tidy
+      int i = 0;
+      while (i < numAmount)
+      {
+        userNumbers = Convert.ToInt32(Console.ReadLine());
+        numbers.Add(userNumbers);
+        i++;
+      }
+
+      // do some calculations (foreach goes through every item, in this case number (int, but can also be a string), in the list)
+      foreach (int number in numbers)
+      {
+        sum = number + sum;
+        total = number * total;
+        average = sum / (double)numAmount; // we want decimals in our average (double)
+      }
+
+      // print number stats
+      Console.WriteLine("Their sum: " + sum);
+      Console.WriteLine("Their total: " + total);
+      Console.WriteLine("Their average: " + average);
     }
   }
 }
